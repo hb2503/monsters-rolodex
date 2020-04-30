9cae04671ddfa4ace5bbc0fe497c7b080c2ce8e0
hb2503 <himanshubanka25@gmail.com>
2020-04-30 19:05:10 +0530

    Updates

---
+++ precache-manifest.21672058b65569ac6c1ddf615612b875.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a87e0522c98cc09424982fa2fcc7d93e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "aeacf6a0f34ca1455e31",
+    "url": "/monsters-rolodex/static/css/main.93dcc940.chunk.css"
+  },
+  {
+    "revision": "3eb42cb93644492131e1",
+    "url": "/monsters-rolodex/static/js/2.2cb16829.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.2cb16829.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aeacf6a0f34ca1455e31",
+    "url": "/monsters-rolodex/static/js/main.a23927a2.chunk.js"
+  },
+  {
+    "revision": "10e2c86637300882bc8a",
+    "url": "/monsters-rolodex/static/js/runtime-main.a8c7c6d0.js"
+  }
+]);
